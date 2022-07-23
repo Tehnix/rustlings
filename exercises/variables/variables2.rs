ca67f6e9e05d774d784de112d2f252663016c56c
Christian Kjær <ckl@codetalk.io>
2022-07-23 21:53:52 +0200

    Go through a few exercises

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,13 +1,16 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+fn type_of<T>(_: T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
 
 fn main() {
-    let x;
+    let x = 2; // inferred to be i32
     if x == 10 {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
     }
+    println!("x is type {}", type_of(x));
 }
